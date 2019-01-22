87f18f5984002b78cea770fe91bb5a65f91cf021
DELHAY <delhayantoine@gmail.com>
2019-01-22 08:40:26 +0100

    Ajout de l'exercice 3

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function red() {
+  document.getElementById("text").style.color = "red";
+}
+
+function blue() {
+  document.getElementById("text").style.color = "blue";
+}
+
+function green() {
+  document.getElementById("text").style.color = "green";
+}
